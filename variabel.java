01d569dbd0e1de5a32f8b58c89d6f2eb7cfb761a
zuroihan <115127481+zuroihan@users.noreply.github.com>
2022-11-24 13:55:58 +0700

    Add files via upload

---
+++ variabel.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    public static void main(String[] args){
+        String nama = "Muhammad Juroihan";
+        int tahun = 2003;
+
+        System.out.println("Assalamualaikum Wr.Wb");
+        System.out.println("Nama Saya ; "+ nama);
+        System.out.println("Saya Lahir di Tahun"+ tahun);
+    }
+}
+
+
+//program ini saya running di https://www.jdoodle.com/online-java-compiler/
+//bahasa pemrograman yang saya pakai yaitu bahasa pemrograman java
